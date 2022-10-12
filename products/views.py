e5a511893119c9e75447337b22ecca7253876997
BLoushi52 <k.albloushii@hotmail.com>
2022-10-12 18:56:32 +0300

    created home view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
+from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def get_home(request):
+    return HttpResponse(f"<h1> Hey, Welcome to our website!</h1>")
